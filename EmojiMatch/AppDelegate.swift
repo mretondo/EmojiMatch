95bd3f892cd2e8fda967e5ce58156520a965a43a
Mike Retondo <mretondo@mac.com>
2022-09-12 16:32:02 -0500

    Made changes to better match Apples example for a color transformer

--- EmojiMatch/AppDelegate.swift
+++ EmojiMatch/AppDelegate.swift
@@ -57,9 +57,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // update the database to reflect themes from above
         // might have deleted some and added new ones
         do {
-            // Transformer for UIColor
-            UIColorValueTransformer.register()
-
             try Themes.updateDatabase(with: themes)
         } catch let error as NSError {
             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -131,6 +128,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
+
+        // Register the transformer at the very beginning.
+        // Transformer for UIColor
+        UIColorValueTransformer.register()
+
         let container = NSPersistentContainer(name: "Model")
         container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
--- EmojiMatch/CoreData/UIColorValueTransformer.swift
+++ EmojiMatch/CoreData/UIColorValueTransformer.swift
@@ -15,9 +15,31 @@ final class UIColorValueTransformer: NSSecureUnarchiveFromDataTransformer {
     /// The name of the transformer. This is the name used to register the transformer using `ValueTransformer.setValueTransformer(_"forName:)`.
     static let name = NSValueTransformerName(rawValue: String(describing: UIColorValueTransformer.self))
 
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+    override class func transformedValueClass() -> AnyClass {
+        return UIColor.self
+    }
+
     // Make sure `UIColor` is in the allowed class list.
     override static var allowedTopLevelClasses: [AnyClass] {
         return [UIColor.self]
+    }
+
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else {
+            fatalError("Wrong data type: value must be a Data object; received \(type(of: value))")
+        }
+        return super.transformedValue(data)
+    }
+
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let color = value as? UIColor else {
+            fatalError("Wrong data type: value must be a UIColor object; received \(type(of: value))")
+        }
+        return super.reverseTransformedValue(color)
     }
 
     /// Registers the transformer.
