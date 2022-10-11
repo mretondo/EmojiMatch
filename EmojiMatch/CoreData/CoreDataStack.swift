e52d0e906a87f7bb8cb4ca1f67a6e7bd9e42108c
Mike Retondo <mretondo@mac.com>
2022-10-10 19:19:03 -0500

    Added code to use pre-filled in CoreData database

--- EmojiMatch/CoreData/CoreDataStack.swift
+++ EmojiMatch/CoreData/CoreDataStack.swift
@@ -2,8 +2,10 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
+    // MARK: Properties
     private let name: String
 
+    // MARK: Initializers
     init(name: String) {
         self.name = name
     }
@@ -12,7 +14,7 @@ class CoreDataStack {
         return persistentContainer.viewContext
     }()
 
-    private lazy var persistentContainer: NSPersistentContainer = {
+    lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
@@ -25,7 +27,8 @@ class CoreDataStack {
         UIColorValueTransformer.register()
 
         let container = NSPersistentContainer(name: name)
-        container.loadPersistentStores { (storeDescription, error) in
+//        self.seedCoreDataContainerIfFirstLaunch()
+        container.loadPersistentStores { storeDescription, error in
             // Avoid duplicating objects - There's a constraint on property 'name'
             // For properties which have been changed in both the external source and in memory, the in memory changes trump the external ones
             container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
@@ -60,4 +63,51 @@ class CoreDataStack {
             fatalError("Unresolved CoreData error \(nserror), \(nserror.userInfo)")
         }
     }
+}
+
+// MARK: Private
+private extension CoreDataStack {
+    //swiftlint:disable force_unwrapping
+    func seedCoreDataContainerIfFirstLaunch() {
+        let previouslyLaunched = UserDefaults.standard.bool(forKey: "previouslyLaunched")
+        if !previouslyLaunched {
+            UserDefaults.standard.set(true, forKey: "previouslyLaunched")
+
+            // Default directory where the CoreDataStack will store its files
+            let directory = NSPersistentContainer.defaultDirectoryURL()
+            let url = directory.appendingPathComponent(name + ".sqlite")
+
+            // Copying the SQLite file
+            let seededDatabaseURL = Bundle.main.url(forResource: name, withExtension: "sqlite")!
+            _ = try? FileManager.default.removeItem(at: url)
+            do {
+                try FileManager.default.copyItem(at: seededDatabaseURL, to: url)
+            } catch let nserror as NSError {
+                fatalError("Error: \(nserror.localizedDescription)")
+            }
+
+            // Copying the SHM file
+            let seededSHMURL = Bundle.main.url(forResource: name, withExtension: "sqlite-shm")!
+            let shmURL = directory.appendingPathComponent(name + ".sqlite-shm")
+            _ = try? FileManager.default.removeItem(at: shmURL)
+            do {
+                try FileManager.default.copyItem(at: seededSHMURL, to: shmURL)
+            } catch let nserror as NSError {
+                fatalError("Error: \(nserror.localizedDescription)")
+            }
+
+            // Copying the WAL file
+            let seededWALURL = Bundle.main.url(forResource: name, withExtension: "sqlite-wal")!
+            let walURL = directory.appendingPathComponent(name + ".sqlite-wal")
+            _ = try? FileManager.default.removeItem(at: walURL)
+            do {
+                try FileManager.default.copyItem(at: seededWALURL, to: walURL)
+            } catch let nserror as NSError {
+                fatalError("Error: \(nserror.localizedDescription)")
+            }
+
+            print("Seeded Core Data")
+        }
+    }
+    //swiftlint:enable force_unwrapping
 }
