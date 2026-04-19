890a9bbae687da9031618e9b8c84d86b2da55171
Mike Retondo <mretondo@mac.com>
2026-04-19 13:38:55 -0500

    Modifications to compile with Swift 6

---
+++ EmojiMatch/CoreData/Score+CoreDataClass 2.swift
@@ -0,0 +1,73 @@
+//
+//  Score+CoreDataClass.swift
+//  Match Emojis
+//
+//  Created by Mike Retondo on 8/24/19.
+//
+
+import CoreData
+
+class Score: NSManagedObject
+{
+    @MainActor
+    public static var highScore: Int64? {
+        get {
+            let moc = AppEnvironment.shared.coreDataStack.moc
+            let request = fetchRequest()
+
+            do {
+                let scores = try moc.fetch(request)
+                assert(scores.count <= 1, "highScore - scores count isn't <= 1")
+
+                if scores.count == 1 {
+                    return scores[0].highScore
+                } else {
+                    return nil
+                }
+            } catch {
+                return nil
+            }
+        }
+
+        set(newValue) {
+            if let newValue = newValue {
+                // get high score from this Entity
+                let highScore = highScore
+
+                if highScore == nil || newValue > highScore! {
+                    let moc = AppEnvironment.shared.coreDataStack.moc
+
+                    // NOTE: no data is retrieved here, the database only retrieves the Entities record count
+                    if let count = try? moc.count(for: Score.fetchRequest()), count == 0 {
+                        // set first high score into empty Entity
+                        let score = Score(context: moc)
+                        score.highScore = newValue
+                    } else {
+                        // modify previous saved high score
+                        let request: NSFetchRequest<Score> = Score.fetchRequest()
+                        do {
+                            if let highScores = try? moc.fetch(request) {
+                                let score = highScores[0]   // there's only one score in the Entity
+                                score.highScore = newValue
+                            }
+                        }
+                    }
+                }
+            }
+
+            printScoreTableStats()
+        }
+    }
+
+    @MainActor
+    public static func printScoreTableStats() {
+        #if DEBUG
+        // no data is retrieved, the database only retrieves the record count
+        if let count = try? AppEnvironment.shared.coreDataStack.moc.count(for: fetchRequest()) {
+            print ("\(count) Score\n")
+        } else {
+            print ("No Score\n")
+        }
+        #endif
+    }
+}
